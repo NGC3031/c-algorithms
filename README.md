39768701c3e7d56770e8a1c9e55ac42bfb775800
NGC3031 <scott.d@ieee.org>
2019-03-22 13:15:52 +1100

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+#C algorithms
+Algorithms in c  that I have used for teaching and self learning over the years. Use at your own risk. For convenience the JS is in script tags where browser output is used but probably better to move code out to it's own js file.
+
+-Live long and prosper.
---
+++ connectivity.c
@@ -0,0 +1,28 @@
+/*
+CONNECTIVITY ALGORITHM - Pairs of numbers represent network connections so
+finds the start and end point of connections.
+eg. 1 3 4 6 3 5     1-3 -> 1-3-5 and 4-6
+Reads a sequence of pairs of nonnegative integers less than N from standard 
+input (interpreting the pair p q to mean “connect object p to object q”) 
+and prints out pairs representing objects that are not yet connected. 
+It maintains an array id that has an entry for each object, with the property 
+that id[p] and id[q] are equal if and only if p and q are connected.  
+N is defined as a compile-time constant. 
+*/
+#include <stdio.h>
+#define N 10000
+main()
+{
+    int i, p, q, t, id[N];
+    for (i = 0; i < N; i++)
+        id[i] = i;
+    while (scanf("%d %d\n", &p, &q) == 2)
+    {
+        if (id[p] == id[q])
+            continue;
+        for (t = id[p], i = 0; i < N; i++)
+            if (id[i] == t)
+                id[i] = id[q];
+        printf(" %d %d\n", p, q);
+    }
+}
